5d2dba8f997d3e0f0c153998ea8e7ba2e7e36142
alexdai186 <alexdai186@gmail.com>
2022-04-25 14:20:53 -0500

    add signs to crabDelta calculation

--- src/crabHelpers.ts
+++ src/crabHelpers.ts
@@ -17,7 +17,7 @@ import { QUOTE_INDEX } from './layout';
 import { BN } from 'bn.js';
 import {readConfig, openConnection} from './cli'
 
-export const CRAB_PUBKEY = 'VKH3Tf7yAgxU5JKkuU7HLmrYCvnGM2LKsPL9bvgRHq3';
+export const CRAB_PUBKEY = 'ASVf3MRC3hpM7tc33r7UVdXN5wowLQoUBd328JPhKaug';
 
 export async function getCrabDelta(pubkey=CRAB_PUBKEY) {
     const config = readConfig(__dirname+'/ids.json' as string);
@@ -54,24 +54,26 @@ export async function getCrabDelta(pubkey=CRAB_PUBKEY) {
     const btc2Quote = btc2PerpAccount.getQuotePosition(
         entropyCache.perpMarketCache[btc2MarketIndex]
     ).toNumber() / quoteAdj.toNumber();
-    const btc2Funding = btc2PerpAccount.getUnsettledFunding(
-        entropyCache.perpMarketCache[btc2MarketIndex]
-    ).toNumber() / quoteAdj.toNumber();
-    // Assumes we are always long the BTC^2 Position
-    const btc2TotalPos = btc2Quote+btc2Funding;
-
+    const btc2Sign = Math.sign(
+        entropyAccount.getBasePositionUiWithGroup(
+            btc2MarketIndex,
+            entropyGroup,
+        )
+    );
 
     const btcPerpAccount = entropyAccount.perpAccounts[btcMarketIndex];
     const btcQuote = btcPerpAccount.getQuotePosition(
         entropyCache.perpMarketCache[btcMarketIndex]
     ).toNumber() / quoteAdj.toNumber();
-    const btcFunding = btcPerpAccount.getUnsettledFunding(
-        entropyCache.perpMarketCache[btcMarketIndex]
-    ).toNumber() / quoteAdj.toNumber();
-    // Assumes we are always short the BTC position.
-    const btcTotalPos = btcQuote-btcFunding;
-    const delta = (btc2TotalPos*2+btcTotalPos)/equity;
+    const btcSign = Math.sign(
+        entropyAccount.getBasePositionUiWithGroup(
+            btcMarketIndex,
+            entropyGroup,
+        )
+    );
+        // Assumes we are always short the BTC position.
+    const delta = (btc2Quote*btc2Sign*2+btcQuote*btcSign)/equity;
     console.log("Delta: ", delta);
     return delta
 }
-// getCrabDelta();
+getCrabDelta();
